e5e6f86f383549516b8f3d291028f3acbb828339
Kreestoff2024 <christophe.lefrancois2024@campus-eni.fr>
2026-03-06 11:29:00 +0100

    Updated pom.xml

--- cura-healthcare/src/test/java/factory/BaseTest.java
+++ cura-healthcare/src/test/java/factory/BaseTest.java
@@ -1,35 +1,42 @@
 package factory;
+
 import io.github.bonigarcia.wdm.WebDriverManager;
-import org.openqa.selenium.Dimension;
 import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.firefox.FirefoxDriver;
-import org.openqa.selenium.firefox.FirefoxOptions;
-import org.testng.annotations.AfterMethod;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+import org.openqa.selenium.Dimension;
 import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.AfterMethod;
+
+import java.time.Duration;
 
 public abstract class BaseTest {
-
     protected WebDriver driver;
 
     @BeforeMethod
-    public void setUp(){
-        String headless = System.getProperty("headless", "false");
-
-        WebDriverManager.firefoxdriver().setup();
-        FirefoxOptions options = new FirefoxOptions();
-        if (headless.equals("true")) {
-            options.addArguments("--headless");
+    public void setUp() {
+        WebDriverManager.chromedriver().setup();
+        ChromeOptions options = new ChromeOptions();
+        String headless = System.getProperty("headless", "true");
+        if ("true".equals(headless)) {
+            options.addArguments("--headless=new");
+            options.addArguments("--disable-gpu");
+            options.addArguments("--no-sandbox");
+            options.addArguments("--disable-dev-shm-usage");
         }
-        driver = new FirefoxDriver(options);
-        driver.manage().window().setSize(new Dimension(1920,1080));
+        options.addArguments("--window-size=1920,1080");
+        driver = new ChromeDriver(options);
+        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(15));
     }
 
     @AfterMethod
-    public void tearDown(){
-        driver.quit();
+    public void tearDown() {
+        if (driver != null) {
+            driver.quit();
+        }
     }
 
-    public void navigateTo(String url){
-        driver.navigate().to(url);
+    protected void navigateTo(String url) {
+        driver.get(url);
     }
 }
